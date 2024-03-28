dbcd2c34747267e31bff40fe5db48cd0f0465390
abhiram <abhiramreddyabc@gmail.com>
2024-03-28 19:22:37 +0530

    Completed testing

--- train.py
+++ train.py
@@ -1,18 +1,13 @@
 import cv2
-import numpy as np
-import pandas as pd
 from ultralytics import YOLO
 import time
-import albumentations as A
 
 start_time = time.time()
 
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov8n.yaml")
 
-# Use the model
-model.train(data="config.yaml", epochs=200, resume = True)  # train the model
-metrics = model.val()  # evaluate model performance on the validation set
-# results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
+model.train(data="config.yaml", epochs=200, resume = True)
+metrics = model.val()
 path = model.export(format="onnx")  # export the model to ONNX format
 
 end_time = time.time()
